57e98fb25925b72fdf527b661d3cfdafa5d43610
Thialala <obarrypublic@gmail.com>
2016-01-03 02:53:52 +0100

    setting up for module

--- GeoLib.Client/MainWindow.xaml.cs
+++ GeoLib.Client/MainWindow.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
 using System.ServiceModel;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using GeoLib.Client.Contracts;
 using GeoLib.Proxies;
-using Binding = System.ServiceModel.Channels.Binding;
 
 namespace GeoLib.Client
 {
@@ -39,10 +24,10 @@ namespace GeoLib.Client
         {
             if (txtZipCode.Text != null)
             {
-                var proxy = new GeoClient("httpEP");
+                var proxy = new GeoClient();
                 var data = proxy.GetZipInfo(txtZipCode.Text);
                 if (data != null)
-                { 
+                {
                     lblCity.Content = data.City;
                     lblState.Content = data.State;
                 }
@@ -55,11 +40,12 @@ namespace GeoLib.Client
         {
             if (txtState != null)
             {
-                EndpointAddress address = new EndpointAddress("net.tcp://localhost:8009/GeoService");
-                Binding binding = new NetTcpBinding();
+                //EndpointAddress address = new EndpointAddress("net.tcp://localhost:8009/GeoService");
+                //Binding binding = new NetTcpBinding();
 
-                GeoClient proxy = new GeoClient(binding, address);
+                //GeoClient proxy = new GeoClient(binding, address);
 
+                var proxy = new GeoClient();
                 var datas = proxy.GetZips(txtState.Text);
 
                 if (datas != null)
